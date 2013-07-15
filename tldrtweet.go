ab2dd7b46578f1c1bf6c0cb527999876b6576ea2
Nathan <nlp125psu@gmail.com>
2013-07-15 16:25:33 -0400

    The bot can go crawl through reddit and tweet out tldr messages, however I still need to add support for ensuring that there are no repeats.

--- tldrtweet.go
+++ tldrtweet.go
@@ -1,38 +1,54 @@
 package main
 
 import (
+	"container/list"
 	"fmt"
 	"github.com/jzelinskie/reddit"
+	"github.com/kurrik/oauth1a"
+	"github.com/kurrik/twittergo"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"os"
 	"strings"
 	"time"
 )
 
 type tldrItem struct {
 	Content string
-	Id      string
+	Author  string
+	Created float64
 }
 
 func main() {
 	posts, err := reddit.SubredditHeadlines("askreddit")
-	if err != nil {
-		panic(err)
-	}
+	handleError(err)
 
+	tldrItemList := list.New()
 	for _, post := range posts {
 		// Sleep for 2 seconds as a niave way to keep the number of hits down to a max of 30 a min		
 		time.Sleep(2 * time.Second)
 		comments, err := reddit.GetComments(post)
-		if err != nil {
-			panic(err)
-		}
+		handleError(err)
 		// Simple search and print of tldr comments
 		for _, comment := range comments {
 			formattedBody := strings.ToLower(comment.Body)
 			found, sentence := extractTLDR(formattedBody)
-			if found {
-				fmt.Println(tldrItem{Content: sentence, Id: comment.FullId})
+			// Only add tweetable items to the list of candidates, i.e. less than 140 characters
+			if found && len(sentence) < 140 {
+				foundItem := tldrItem{Content: sentence, Author: comment.Author, Created: comment.Created}
+				fmt.Printf("%v\n", foundItem)
+				tldrItemList.PushBack(foundItem)
 			}
 		}
+	}
+
+	if tldrItemList.Len() > 0 {
+		tweetItem := tldrItemList.Front()
+		client := LogIn()
+		message := tweetItem.Value.(tldrItem).Content
+		fmt.Println(message)
+		TweetMessage(message, client)
 	}
 }
 
@@ -57,4 +73,42 @@ func extractTLDR(body string) (bool, string) {
 		}
 	}
 	return false, ""
+}
+
+// The login, loading of credentials, and tweeting has been addapted from the example provided with github.com/kurrik/twittergo
+func LoadTwitterCredentials() (client *twittergo.Client, err error) {
+	credentials, err := ioutil.ReadFile("CREDENTIALS")
+	handleError(err)
+	lines := strings.Split(string(credentials), "\n")
+	config := &oauth1a.ClientConfig{
+		ConsumerKey:    lines[0],
+		ConsumerSecret: lines[1],
+	}
+	user := oauth1a.NewAuthorizedConfig(lines[2], lines[3])
+	client = twittergo.NewClient(config, user)
+	return
+}
+
+func LogIn() *twittergo.Client {
+	client, err := LoadTwitterCredentials()
+	handleError(err)
+	return client
+}
+
+func TweetMessage(message string, client *twittergo.Client) {
+	data := url.Values{}
+	data.Set("status", message)
+	body := strings.NewReader(data.Encode())
+	req, err := http.NewRequest("POST", "/1.1/statuses/update.json", body)
+	handleError(err)
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	_, err = client.SendRequest(req)
+	handleError(err)
+}
+
+func handleError(err error) {
+	if err != nil {
+		fmt.Printf("%v", err)
+		os.Exit(1)
+	}
 }
